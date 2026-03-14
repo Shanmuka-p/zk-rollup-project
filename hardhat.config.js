60a84d83cbe06f8fb7f65af3d722c54ee95aae06
Shanmuka-p <shanmukareddypadala@gmail.com>
2026-03-14 15:30:52 +0530

    chore(config): downgrade hardhat-toolbox for compatibility and configure local network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545", // Changed back so Windows can find it!
+      chainId: 31337,
+    },
+    hardhat: {}
+  }
 };
